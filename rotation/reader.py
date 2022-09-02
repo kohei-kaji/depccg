f62cbaa9dfe44b01d9ddc13f7715dd08c537bb96
kohei-kaji <kohei.kajikawa1223@gmail.com>
2022-09-02 23:23:07 +0900

    fix to discard an unnecessary brace in op_symbol

--- rotation/reader.py
+++ rotation/reader.py
@@ -80,7 +80,7 @@ class ParsedJaCCGLineReader(object):
 
     def parse_tree(self) -> Tree:
         self.check('{')
-        op_string = self.next(' ')
+        op_symbol = self.next(' ')
         # cat = DEPENDENCY.sub('', self.next(' '))
         cat = self.next(' ')
         cat = Category.parse(cat)
@@ -95,9 +95,9 @@ class ParsedJaCCGLineReader(object):
         self.next('}')
 
         if len(children) == 1:
-            return Tree.make_unary(cat, children[0], op_string, op_string)
+            return Tree.make_unary(cat, children[0], op_symbol.replace("{", ""), op_symbol.replace("{", ""))
         else:
             assert len(
                 children) == 2, f'failed to parse, invalid number of children: {self.line}'
             left, right = children
-            return Tree.make_binary(cat, left, right, op_string.replace("{", ""), op_string.replace("{", ""))
+            return Tree.make_binary(cat, left, right, op_symbol.replace("{", ""), op_symbol.replace("{", ""))
