a7699b69087403f51f954762e1412e34315172cb
kohei-kaji <kohei.kajikawa1223@gmail.com>
2022-09-10 13:24:20 +0900

    create nodecount data or auto format treebank

--- rotation/__main__.py
+++ rotation/__main__.py
@@ -13,11 +13,10 @@ from pathlib import Path
 from depccg.tools.ja.reader import read_ccgbank
 
 from converter import ja_to_auto
-from depccg.tree import Tree
 from nodecount import nodecount
 from reader import read_parsedtree
-from rotation import TreeRotation 
 from typeraise import TypeRaise
+from tree_rotation import TreeRotation
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -30,39 +29,37 @@ if __name__ == '__main__':
     args = parser.parse_args()
     parent = str(Path(args.FILE).parent)
     file = str(Path(args.FILE).stem)
-    directory_name = parent + '/' + file
-    
-    os.mkdir(directory_name)
 
     if args.input == 'parsed':
+        directory_name = parent + '/' + file + '_nc'
+        os.makedirs(directory_name, exist_ok=True)
+        
         trees = [tree for _, _, tree in read_parsedtree(args.FILE)]
-        nd_name = directory_name + '_nodecount.csv'
-        nodecount(nd_name, trees)
+        nc_name = directory_name + '/right.csv'
+        nodecount(nc_name, trees)
         
         typeraised_trees = [TypeRaise.apply_typeraise(tree) for tree in trees]
-        tr_nd_name = directory_name + 'typeraised_nodecount.csv'
-        nodecount(tr_nd_name, typeraised_trees)
+        tr_nc_name = directory_name + '/typeraised.csv'
+        nodecount(tr_nc_name, typeraised_trees)
         
         leftbranched_trees = [TreeRotation.rotate(tree) for tree in trees]
-        lb_nd_name = directory_name + 'leftbranched_nodecount.csv'
+        lb_nd_name = directory_name + '/leftbranched.csv'
         nodecount(lb_nd_name, leftbranched_trees)
         
         shutil.move(args.FILE, directory_name)
             
     else:
+        directory_name = parent + '/' + file + '_auto'
+        os.makedirs(directory_name, exist_ok=True)
+        
         trees = [tree for _, _, tree in read_ccgbank(args.FILE)]
-        auto_name = directory_name + '_auto'
+        auto_name = directory_name + '/right'
         ja_to_auto(auto_name, trees)
         
         typeraised_trees = [TypeRaise.apply_typeraise(tree) for tree in trees]
-        tr_auto_name = directory_name + 'typeraised_auto'
-        nodecount(tr_auto_name, typeraised_trees)
+        tr_auto_name = directory_name + '/typeraised'
+        ja_to_auto(tr_auto_name, typeraised_trees)
         
         leftbranched_trees = [TreeRotation.rotate(tree) for tree in trees]
-        lb_auto_name = directory_name + 'leftbranched_auto'
-        nodecount(lb_auto_name, leftbranched_trees)
-        
-        shutil.move(args.FILE, directory_name)
-
-    
-    
+        lb_auto_name = directory_name + '/leftbranched'
+        ja_to_auto(lb_auto_name, leftbranched_trees)
