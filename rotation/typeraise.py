f537bb79108faf159a5bbfb0845e6c8ddd232c6f
kohei-kaji <kohei.kajikawa1223@gmail.com>
2022-10-12 00:49:02 +0900

    tiny change

--- rotation/typeraise.py
+++ rotation/typeraise.py
@@ -45,19 +45,6 @@ def typeraise(l: Category, r: Functor) -> Functor:
     r = clear_features(r)
     return Functor(r.left, '/', Functor(r.left, '\\', l))
 
-# import re
-# def is_vp(cat: Category) -> bool:
-#     """
-#     A functor category is VP when
-#         1. it includes just one 'S' category and
-#         2. it starts with 'S' category and
-#         3. it has no forward slash ('/').
-#     """
-#     s = str(cat)
-#     return (cat.is_functor
-#             and s.count('S') == 1
-#             and re.match(r'\(*S', s) is not None
-#             and re.search(r'/', s) is None)
 
 class TypeRaise(object):
     def __init__(self, filepath: str) -> None:
