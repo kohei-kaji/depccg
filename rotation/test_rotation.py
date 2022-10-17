ae5738c8a013ff52c44f466a0d42cbf1a61937b8
kohei-kaji <kohei.kajikawa1223@gmail.com>
2022-10-17 22:26:00 +0900

    add a sentence

--- rotation/test_rotation.py
+++ rotation/test_rotation.py
@@ -10,7 +10,8 @@ if __name__ == '__main__':
               r'{> S {ADV0 S/S {S 1/1/_/_}} {< S {NP 2/2/_/_} {S\NP 3/3/_/_}}}',
               r'{> S {S/S 1/1/_/_} {< S {NP 2/2/_/_} {<B1 S\NP {S\NP 3/3/_/_} {S\S 4/4/_/_}}}}',
               r'{> S {> S/S {(S/S)/(S\NP) 1/1/_/_} {> S\NP {(S\NP)/NP 2/2/_/_} {NP 3/3/_/_}}} {> S {S/NP 4/4/_/_} {NP 5/5/_/_}}}',
-              r'{< NP {NP 1/1/_/_} {<B1 NP\NP {NP\NP 2/2/_/_} {NP\NP 3/3/_/_}}}']
+              r'{< NP {NP 1/1/_/_} {<B1 NP\NP {NP\NP 2/2/_/_} {NP\NP 3/3/_/_}}}',
+              r'{> NP {>B NP/NP {NP/NP 池田/池田/_/_} {>B NP/NP {NP/NP 弘子/弘子/_/_} {NP/NP 先生/先生/_/_}}} {< NP {> NP {NP/NP （/（/_/_} {> NP {NP/NP ７/７/_/_} {NP ５/５/_/_}}} {NP\NP ）/）/_/_}}}}']
 
     for i,j in enumerate(read_parsedstring(inputs)):
         print(i)
